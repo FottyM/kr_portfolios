a9dbc97c6d1c4ab9d77cd1b8368f07699c6ea0be
Fortunat Mutunda <fotimutunda@gmail.com>
2018-07-23 01:21:00 +0300

    github deploy

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 import isNil from 'lodash/isNil'
 import PropTypes from 'prop-types'
